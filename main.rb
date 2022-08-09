9fb1509ec39183fe8b8325073b7ce0729afc95f8
Travis Williams <traviswilliams997@gmail.com>
2022-08-09 18:48:32 -0500

    Create function for player1 to choose code

--- main.rb
+++ main.rb
@@ -11,11 +11,31 @@
             puts " "
           end
     end
+
      
  
   end
   class Player1
+    def initialize
+    end
+    def choose_code
+        puts "You can choose from six colours for your code"
+        puts "Colours are: Red, Blue, Yellow, Orange, Purple, Green"
+        puts "Enter first colour"
+        @colour1 = gets.chomp
+        puts "Enter second tcolour"
+        @colour2 = gets.chomp
+        puts "Enter third colour"
+        @colour3 = gets.chomp
+        puts "Enter fourth colour"
+        @colour4 = gets.chomp
+
+      @code = [@colour1, @colour2, @colour3, @colour4]
+      p @code
+    end
   
   end
   new_game = Board.new
-  new_game.current_board
+  #new_game.current_board
+  p1 = Player1.new
+  p1.choose_code
