d33d258423ba73154a524e2d8b5cba17d33b2433
Travis Williams <traviswilliams997@gmail.com>
2022-08-09 18:36:33 -0500

    Create function to display current board state

--- main.rb
+++ main.rb
@@ -1,9 +1,21 @@
   class  Board
     def initialize
-        board_state = [["x","x","x","x"], ["x","x","x","x"], ["x","x","x","x"]]    
+        @board_state = [["x","x","x","x"], ["x","x","x","x"], ["x","x","x","x"]]    
+    end
+    def current_board
+        @board_state.each do |row|
+            row.each do |col|
+              print col
+              print " "
+            end
+            puts " "
+          end
     end
      
  
   end
   class Player1
+  
   end
+  new_game = Board.new
+  new_game.current_board
