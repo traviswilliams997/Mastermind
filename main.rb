fd2caf8e0145daf8de505161d854d2ed8c9f8b17
Travis Williams <traviswilliams997@gmail.com>
2022-08-10 18:41:15 -0500

    Fix check guess function

--- main.rb
+++ main.rb
@@ -16,6 +16,8 @@
  
   end
   class Player1
+    attr_accessor :code
+
     def initialize
     end
     def choose_code
@@ -23,7 +25,7 @@
         puts "Colours are: Red, Blue, Yellow, Orange, Purple, Green"
         puts "Enter first colour"
         @colour1 = gets.chomp
-        puts "Enter second tcolour"
+        puts "Enter second colour"
         @colour2 = gets.chomp
         puts "Enter third colour"
         @colour3 = gets.chomp
@@ -37,6 +39,8 @@
   end
 
   class Player2
+    attr_accessor :guess
+
     def initialize
     end
     def guess_code
@@ -62,18 +66,29 @@ end
         @p2 = Player2.new
         @p1.choose_code
        
-        @p2.guess_code 
-        
-        if @code == @guess
-        p "Win"
-    else
-      p "Lose"
-    end       
+        @p2.guess_code      
      
+        end
+        def check_guess
+          win = true
+          
+          for i in 0..3 do  
+            
+            unless @p1.code[i] == @p2.guess[i]
+                win = false
+            end
+
+          end
+          if win 
+            p "Win"
+          else
+            p "Lose"
+          end
         end
        
        
     end
     new_game = Game.new
+    new_game.check_guess
 
  
